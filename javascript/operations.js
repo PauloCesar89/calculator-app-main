d7660ffcddb9531f08800f780b02a06ddb6d9587
paulocesar89 <pc_schmitz@outlook.com>
2023-02-22 15:29:23 -0300

    calcular dados digitados e exibe na tela

--- javascript/operations.js
+++ javascript/operations.js
@@ -2,3 +2,13 @@ function insert(num){
     var number = document.getElementById('result').innerHTML;
     document.getElementById('result').innerHTML = number + num;
 }
+
+function operationCalc(){
+    var result = document.getElementById('result').innerHTML;
+    if(result){
+    document.getElementById('result').innerHTML = eval(result);
+}else{
+    document.getElementById('result').innerHTML = ("erro sintaxe");
+}
+
+}
