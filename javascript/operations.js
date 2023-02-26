c6feef358ee0afd8179242abe8eafa1f421c909e
paulocesar89 <pc_schmitz@outlook.com>
2023-02-25 23:59:49 -0300

    funcao tema

--- javascript/operations.js
+++ javascript/operations.js
@@ -22,3 +22,13 @@ function del(){
     var result = document.getElementById('result').innerHTML;
     document.getElementById('result').innerHTML = result.substring(0,result.length-1);
 }
+
+function themeSecond(){
+    var main = document.querySelector('main');
+    var theme2 = document.querySelector('themeSecond');
+
+    if(document.getElementById('themeSelect').innerHTML.value = 2){
+
+        document.getElementById("main")[0].style.backgroundColor = 'blue';
+    }
+}
